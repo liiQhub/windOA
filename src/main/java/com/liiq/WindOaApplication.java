e045473ae1c3ae62b2b507e98da1894781395dac
liiQhub <41280264+liiQhub@users.noreply.github.com>
2018-07-20 20:10:06 +0800

    D0720

---
+++ src/main/java/com/liiq/WindOaApplication.java
@@ -0,0 +1,12 @@
+package com.liiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindOaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindOaApplication.class, args);
+	}
+}
